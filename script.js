e9f8d1d36549b61d2ce148d7add6d8a32da897d8
andrewskozak <andrewkozak@ocadu.ca>
2026-03-14 22:57:20 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+var buildings = [
+  {
+    name: "Eiffel Tower",
+    location: "Paris, France",
+    status: "OPEN",
+    restoration: "20th repainting and elevator overhaul",
+    coords: [48.8584, 2.2945]
+  },
+
+  {
+    name: "Notre-Dame",
+    location: "Paris, France",
+    status: "RESTORING",
+    restoration: "nothing",
+    coords: [48.8530, 2.3499]
+  }
+
+];
